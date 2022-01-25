ffceabe8e96a5fe0c5e4ceabfc5c5d37f91ae0d9
Chris M <cm740@exeter.ac.uk>
2022-01-25 13:59:58 +0000

    Rearrange docker setup

--- api-gateway/routes/index.js
+++ api-gateway/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("Successful")
+  res.send("Successful, but with buildpacks! and lots of live reload!")
 });
 
 module.exports = router;
