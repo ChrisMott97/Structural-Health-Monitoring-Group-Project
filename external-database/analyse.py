2568d6f6bbd4d27498e554ff1039be6a1bff9410
Chris M <cm740@exeter.ac.uk>
2022-02-01 20:39:44 +0000

    Updated basic database analysis that outputs .md

--- external-database/analyse.py
+++ external-database/analyse.py
@@ -1,28 +1,27 @@
-from math import sqrt
-from numpy import average, int0
+from numpy import average
 import pymysql.cursors
 import pandas as pd
 import datetime
-from time import ctime
 
 def OrdinalToDatetime(ordinal):
-    plaindate = datetime.date.fromordinal(int(ordinal))
+    try:
+        plaindate = datetime.date.fromordinal(int(ordinal))
+    except:
+        plaindate = datetime.date.fromordinal(1)
     date_time = datetime.datetime.combine(plaindate, datetime.datetime.min.time())
     return date_time + datetime.timedelta(days=ordinal-int(ordinal))
 
 def transform_timestamp(stamp):
-        if type(stamp) == list:
-            result = []
-            for i in stamp:
-                try:
-                    result.append(OrdinalToDatetime(i/(24*3600*1000)).strftime('%d-%m-%Y %H:%M:%S.%f'))
-                except:
-                    result.append(i)
-            return result
         try:
             return OrdinalToDatetime(stamp/(24*3600*1000)).strftime('%d-%m-%Y %H:%M:%S.%f')
         except:
-            return 1
+            return stamp
+
+def transform_time_only(stamp):
+        try:
+            return OrdinalToDatetime(stamp/(24*3600*1000)).strftime('%H:%M:%S.%f')
+        except:
+            return stamp
 
 def get_min(cursor, table):
     sql = "SELECT * FROM `{0}` WHERE `timestamp` = (SELECT MIN(`timestamp`) FROM `{0}`) LIMIT 1".format(table)
@@ -44,8 +43,7 @@ def get_first_x(cursor, table, x):
     cursor.execute(sql)
     result = cursor.fetchall()
     result = [res['timestamp'] for res in result]
-    print(result)
-    return transform_timestamp(result)
+    return [transform_timestamp(i) for i in result]
 
 def avg_timestep(cursor, table, sample):
     sql = "SELECT * FROM `{0}` LIMIT {1}".format(table, sample)
@@ -55,17 +53,19 @@ def avg_timestep(cursor, table, sample):
     avgs = []
     for i in range(1,sample-1):
         avgs.append(result[i+1]-result[i])
-    return transform_timestamp(average(avgs))
+    # print(average(avgs))
+    return transform_time_only(average(avgs))
 
-def print_stats(cursor, table):
-    print("### {0}".format(table.title()))
-    print(" - Min: {0}".format(get_min(cursor, table)))
-    print(" - Max: {0}".format(get_max(cursor, table)))
-    print(" - First 5: ")
+def output_stats(cursor, table):
+    file = open("stats.md", "a") 
+    file.write("### {0}\n".format(table.title()))
+    file.write(" - Min: {0}\n".format(get_min(cursor, table)))
+    file.write(" - Max: {0}\n".format(get_max(cursor, table)))
+    file.write(" - First 5: \n")
     for i in get_first_x(cursor, table, 5):
-        print("\t - {0}".format(i))
-    print(" - Average timestep (minutes:seconds.microseconds): \n\t - {0}".format(avg_timestep(cursor, table, 1000)))
-    print()
+        file.write("    - {0}\n".format(i))
+    file.write(" - Average timestep (hours:minutes:seconds.microseconds): \n    - {0}\n\n".format(avg_timestep(cursor, table, 1000)))
+    file.close() 
 
 # Connect to the database '192.168.32.3'
 connection = pymysql.connect(host='192.168.32.3',
@@ -80,8 +80,12 @@ with connection:
         important_tables = ['displacements', 'gps', 'inclination', 'temperature', 'summary']
         extended_tables = ['extension_exb077ed','extension_exb077wd','extension_exh077ed','extension_exh077wd', 'meteo_hbb', 'meteo_noaa', 'meteo_scada', 'rot_acc']
         
+        file = open("stats.md", "w") 
+        file.write("")
+        file.close()
+
         for i_tab in important_tables:
-            print_stats(cursor, i_tab)
+            output_stats(cursor, i_tab)
         for e_tab in extended_tables:
-            print_stats(cursor, e_tab)
+            output_stats(cursor, e_tab)
 
