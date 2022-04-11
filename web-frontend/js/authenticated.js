c78b7429da4d03f58b69f3b11d6a84ddd6b4077d
Chris M <cm740@exeter.ac.uk>
2022-04-11 21:42:29 +0100

    Basic auth implementation

---
+++ web-frontend/js/authenticated.js
@@ -0,0 +1,40 @@
+// required for authentication header use
+axios.defaults.withCredentials = true;
+// tries to send authentication header with every request
+
+
+let auth0 = null;
+let isAuthenticated = false;
+
+const configureClient = async () => {
+    auth0 = await createAuth0Client({
+        "domain": "***REMOVED***",
+        "client_id": "***REMOVED***",
+        "cacheLocation": "localstorage",
+        "audience": "shm"
+    });
+};
+
+window.onload = async () => {
+    await configureClient();
+    isAuthenticated = await auth0.isAuthenticated();
+
+    if (isAuthenticated) {
+        const token = await auth0.getTokenSilently()
+        axios.get("http://localhost:3030/sensors/GPH000EDE", {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        })
+        .then(function(response){
+            // console.log(response.data)
+            document.getElementById("data").innerText = JSON.stringify(response.data)
+        }).catch(function(error){
+            console.log("Data collection failed")
+        })
+        return;
+    }
+    // Redirect to / if not logged in
+    window.location.replace('/')
+};
+
