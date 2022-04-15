ebcc724d58d1020f9a514a78f75d5320342d7ed1
Chris M <cm740@exeter.ac.uk>
2022-04-15 18:29:42 +0100

    Enable ExpressJS to render views with EJS

--- web/app.js
+++ web/app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors')
+var ejs = require('ejs')
 const { auth, requiredScopes } = require('express-oauth2-jwt-bearer');
 
 var index = require('./controllers/index');
@@ -13,6 +14,8 @@ var anomalies = require('./controllers/anomalies')
 var comments = require('./controllers/comments')
 
 var app = express();
+app.set('view engine', 'html')
+app.engine('html', ejs.renderFile);
 var corsOptions = {
     origin: 'http://localhost',
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
@@ -37,10 +40,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/sensors',checkJwt, sensors);
-app.use('/users',checkJwt, users);
-app.use('/data',checkJwt, data);
-app.use('/anomalies',checkJwt, anomalies);
-app.use('/comments',checkJwt, comments);
+app.use('/api/sensors',checkJwt, sensors);
+app.use('/api/users',checkJwt, users);
+app.use('/api/data',checkJwt, data);
+app.use('/api/anomalies',checkJwt, anomalies);
+app.use('/api/comments',checkJwt, comments);
 
 module.exports = app;
--- web/controllers/index.js
+++ web/controllers/index.js
@@ -3,9 +3,32 @@ var router = express.Router();
 const csv = require('csv-parser')
 const fs = require('fs')
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Done')
+  res.render('index')
+});
+
+router.get('/adduser', function(req, res, next) {
+  res.render('adduser')
+});
+
+router.get('/authenticated', function(req, res, next) {
+  res.render('authenticated')
+});
+
+router.get('/dash', function(req, res, next) {
+  res.render('dash')
+});
+
+router.get('/database', function(req, res, next) {
+  res.render('database')
+});
+
+router.get('/notifications', function(req, res, next) {
+  res.render('notifications')
+});
+
+router.get('/sensor', function(req, res, next) {
+  res.render('sensor')
 });
 
 module.exports = router;
