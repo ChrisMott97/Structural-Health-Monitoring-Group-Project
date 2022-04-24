6e9b1020816bce7ae06e972ed8c82bf26ce75b98
dt365 <dt365@exeter.ac.uk>
2022-04-24 22:04:19 +0000

    Revert "Autoencoder base functionality"
    This reverts merge request !10

--- web/database/development/migrations/20220215102644_create_related_table.js
+++ web/database/development/migrations/20220215102644_create_related_table.js
@@ -2,7 +2,6 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-<<<<<<< HEAD:web/database/development/migrations/20220215102644_create_related_table.js
 exports.up = (knex) =>
   knex.schema.createTable('related', (table) => {
     table.primary(['sensor_id', 'related_id']);
@@ -13,24 +12,9 @@ exports.up = (knex) =>
       .inTable('sensors')
       .notNullable();
   });
-=======
-exports.up = function(knex) {
-  return knex.schema.createTable('related', table => {
-      table.primary(['sensor_id', 'related_id'])
-      table.string('sensor_id').references('id').inTable('sensors').notNullable()
-      table.string('related_id').references('id').inTable('sensors').notNullable()
-  })
-};
->>>>>>> auto-encoder:api-gateway/database/development/migrations/20220215102644_create_related_table.js
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-<<<<<<< HEAD:web/database/development/migrations/20220215102644_create_related_table.js
 exports.down = (knex) => knex.schema.dropTable('related');
-=======
-exports.down = function(knex) {
-  return knex.schema.dropTable('related')
-};
->>>>>>> auto-encoder:api-gateway/database/development/migrations/20220215102644_create_related_table.js
--- web/database/test/seeds/00-reset.js
+++ web/database/test/seeds/00-reset.js
@@ -1,6 +1,5 @@
 /**
  * @param { import("knex").Knex } knex
-<<<<<<< HEAD:web/database/test/seeds/00-reset.js
  * @returns { Promise<void> }
  */
 exports.seed = async function (knex) {
@@ -12,17 +11,3 @@ exports.seed = async function (knex) {
   await knex("users").del();
   await knex("sensors").del();
 };
-=======
- * @returns { Promise<void> } 
- */
- exports.seed = async function(knex) {
-    // Deletes ALL existing entries
-    await knex('comments').del()
-    await knex('related').del()
-    await knex('anomalies').del()
-    await knex('data').del()
-    await knex('users').del()
-    await knex('sensors').del()   
-  };
-  
->>>>>>> auto-encoder:api-gateway/database/test/seeds/00-reset.js
