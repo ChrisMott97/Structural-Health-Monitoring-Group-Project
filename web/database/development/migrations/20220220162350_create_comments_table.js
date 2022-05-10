1de854289966ee470e7780663542e007aa753a75
Chris M <cm740@exeter.ac.uk>
2022-05-10 13:08:13 +0100

    More fixes

--- web/database/development/migrations/20220220162350_create_comments_table.js
+++ web/database/development/migrations/20220220162350_create_comments_table.js
@@ -16,7 +16,7 @@ exports.up = function (knex) {
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
   });
-
+}
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
