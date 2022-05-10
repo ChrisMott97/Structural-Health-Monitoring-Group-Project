51a8689b313edd7f65aae079fb5f0a870207fc32
Chris M <cm740@exeter.ac.uk>
2022-05-10 17:46:19 +0100

    Reduce external database size

--- web/database/development/migrations/20220215101537_create_anomalies_table.js
+++ web/database/development/migrations/20220215101537_create_anomalies_table.js
@@ -6,15 +6,16 @@ exports.up = function (knex) {
   return knex.schema.createTable("anomalies", (table) => {
     table.increments("id");
     table.integer("status").notNullable().defaultTo(1);
-    table.float("confidence").notNullable();
+    table.float("confidence");
     table.float("sensitivity").notNullable();
     table.string("user_id");
     table.timestamp("sensor_time", { precision: 0 }).notNullable();
     table.string("sensor_id");
-    table
-      .foreign(['sensor_time', 'sensor_id'])
-      .references(['time', 'sensor_id'])
-      .inTable('data');
+    table.foreign('sensor_id').references('id').inTable('sensors');
+    // table
+    //   .foreign(['sensor_time', 'sensor_id'])
+    //   .references(['time', 'sensor_id'])
+    //   .inTable('data');
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.timestamp('updated_at').defaultTo(knex.fn.now());
   });
