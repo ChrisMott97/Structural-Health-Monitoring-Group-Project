530f50d33078ec67f2c5cb5bad165b5031404729
DanielTighe <danieltighe@btinternet.com>
2022-05-11 09:56:49 +0100

    Remove unneeded documentation file

--- machine-learning/auto-encoder/auto_encoder.py
+++ machine-learning/auto-encoder/auto_encoder.py
@@ -1,9 +1,7 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
 import pymysql
 import psycopg2
 
