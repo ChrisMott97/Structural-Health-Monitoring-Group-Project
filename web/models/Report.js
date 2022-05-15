8406f57882e437e48a55e8d5892e2465d11a7a90
Chris M <cm740@exeter.ac.uk>
2022-05-15 16:28:33 +0100

    Add user deletion

--- web/models/Report.js
+++ web/models/Report.js
@@ -14,21 +14,32 @@ function findOne(id) {
 }
 
 function create(title, start_date, end_date, sensitivity, user, sensors) {
-  sensors = JSON.parse(sensors)
-  return knex('reports').insert({
-    title,
-    user,
-    sensitivity,
-    start_date,
-    end_date
-  }).returning('id').then((res)=>{
-    const id = res[0].id
-    // console.log(id)
-    let sensors_list = []
-    sensors.forEach(s => {
-      sensors_list.push({sensor_id: s, report_id: id})
-    });
-    return knex('report_sensors').insert(sensors_list).then(()=>{return id})
+  return new Promise((resolve, reject)=>{
+    if(sensors){
+      sensors = JSON.parse(sensors)
+    }
+    errored = true
+    if(title && start_date && end_date && sensitivity && user && sensors){
+      errored = false
+    }
+    if(errored){
+      reject('Some required fields not given.')
+    }
+    return knex('reports').insert({
+      title,
+      user,
+      sensitivity,
+      start_date,
+      end_date
+    }).returning('id').then((res)=>{
+      const id = res[0].id
+      // console.log(id)
+      let sensors_list = []
+      sensors.forEach(s => {
+        sensors_list.push({sensor_id: s, report_id: id})
+      });
+      return knex('report_sensors').insert(sensors_list).then(resolve(id))
+    })
   })
 }
 
