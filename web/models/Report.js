719110f68b225f6859ce3587a494877d331d415e
Chris M <cm740@exeter.ac.uk>
2022-05-15 15:18:08 +0100

    Add report handling

--- web/models/Report.js
+++ web/models/Report.js
@@ -13,4 +13,23 @@ function findOne(id) {
   return knex('reports').select().where({ id }).first();
 }
 
-module.exports = { find, findOne };
+function create(title, start_date, end_date, sensitivity, user, sensors) {
+  sensors = JSON.parse(sensors)
+  return knex('reports').insert({
+    title,
+    user,
+    sensitivity,
+    start_date,
+    end_date
+  }).returning('id').then((res)=>{
+    const id = res[0].id
+    // console.log(id)
+    let sensors_list = []
+    sensors.forEach(s => {
+      sensors_list.push({sensor_id: s, report_id: id})
+    });
+    return knex('report_sensors').insert(sensors_list).then(()=>{return id})
+  })
+}
+
+module.exports = { find, findOne, create };
